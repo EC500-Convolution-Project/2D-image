b1b4232e7bbf3ed69a532c0b734fb8cb94683a29
Patrick Dillon <patrick.dillon@gmail.com>
2018-04-22 11:19:14 -0400

    Added CImg library for opening images and basic example looping through pixels. Need to incorporate with existing function signatures (e.g. existing functions use pointers to doubles.)

--- Makefile
+++ Makefile
@@ -1,17 +1,17 @@
-CC=g++
-
-OBJS=main.o conv2D.o
+CC = g++
+OBJS = main.o conv2D.o
+FLAGS = -lm -lpthread
 
 default: convolution2D
 
 convolution2D: $(OBJS)
-	$(CC) -o convolution2D $(OBJS) 
+	$(CC) -o convolution2D $(OBJS) $(FLAGS)
 
 conv2D.o: conv2D.cpp conv2D.h
 	$(CC) -c conv2D.cpp -o conv2D.o 
-	
-main.o: main.cpp conv2D.h
-	$(CC) -c main.cpp
+
+main.o: main.cpp conv2D.h 
+	$(CC) -c main.cpp 
 
 clean: 
 	rm $(OBJS) convolution2D
--- main.cpp
+++ main.cpp
@@ -5,12 +5,24 @@
 	Group 1
 */
 
+#define cimg_display 0
 #include "conv2D.h"
+#include "CImg.h"
+
 using namespace std;
+using namespace cimg_library;
 
 int main(int argc, char** argv)
 {
-	
+  CImg<double> img("image.bmp");
+
+  /* CImg defines macros for looping over images. Examples:
+     http://cimg.eu/reference/group__cimg__loops.html
+  */
+  cimg_forXY(img,x,y) {
+    cout << "x: " << x << " y: " << y << " pixel: " << img(x,y) << endl;
+  }
+  img.save("new_image.bmp");
 	
 
 	return 0;
